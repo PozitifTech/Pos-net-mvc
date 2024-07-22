49834066e71437f77b7cfff996eb2ebe97edbc2f
Mahmut Celik <mahmut.celik@pozitiftech.com>
2024-07-22 14:52:46 +0300

    IPA-4913 PreAuth Completed

---
+++ PosFix.DeveloperPortal.Core/Response/PreAuthResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosFix.DeveloperPortal.Core.Response
+{
+    public class PreAuthResponse:BaseResponse
+    {
+        public String Amount;
+        public String OrderId;
+        public String CommissionRate;
+    }
+}
